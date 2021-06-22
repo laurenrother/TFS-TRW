6ec803d38fe8999ed14be36a454d101a77bd5f9f
Lauren Rother <lauren@hashicorp.com>
2021-06-22 13:57:23 -0400

    Update example.tf

--- example.tf
+++ example.tf
@@ -3,8 +3,25 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-02bcbb802e03574ba"
-  region        = "us-east-2"
   instance_type = "t2.micro"
+module "vpc" {
+  source  = "laurenrother/vpc/aws"
+  version = "3.1.1"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 }
 
 # Adding a comment, any comment
